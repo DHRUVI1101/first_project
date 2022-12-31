ae2bdfa863ef62b4abde91e1c703b2b4675811df
Dhruvi Dhameliya <d.jdhameliya0090@gmail.com>
2022-12-31 15:02:47 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports = {
+    a: 56,
+    b: 60,
+    c: 70,
+    d: function () {
+        return 10;
+    }
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+// colors package :-
+const colors = require('colors');
+
+console.log("Hello Everyone..........!".rainbow);
+//--------------------------------Basic Server---------------------------------
+// method 1 :-
+// http.createServer((req,res) => {
+//     res.write("Hello Students......!");c
+//     res.end();
+// }).listen(4500);
+
+// method 2 :-
+// function dataControl (req,res)
+// {
+//     res.write("<h1>Hello Students, Welcome to simba Institute......!</h1>");
+//     res.end();
+// }
+
+// http.createServer(dataControl).listen(4500);
+
+// Arrow function:-
+const dataControl = (req,res) => {
+    res.write("<h1>Hello Students, Welcome to Simba Institute......!</h1>");
+    res.end();
+}
+
+http.createServer(dataControl).listen(4600);
